83082767f2c5f17f0767292f4784ae267d1d08ad
Mauricio Rojas <maurir51@outlook.com>
2022-10-24 21:22:25 -0300

    Adds events GET endpoint

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express')
 
 const app = express()
 
-const port = 3000
+const port = 4000
 
 app.get('/users', (req, res) => {
     res.json({ message: 'Users API'})
 })
 
+app.get('/events', (req, res) => {
+    res.json({ message: 'Events API' })
+})
 
 app.listen(port, () => {
     console.log('App running in port', port)
